0e76c234d4d872643055b0d1e7879eb22e7f2d49
Viten3d <90654400+Viten3d@users.noreply.github.com>
2025-09-20 02:44:23 +0000

    xd

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
+#Bind a string to a name
+test_string = "apple"
+
+#Create a variable with an integer value
+test_integer = "818"
+
+#Create a list with various data types
+test_list = [2.718, "book", True, 1706]
+
+#Create an array
 
