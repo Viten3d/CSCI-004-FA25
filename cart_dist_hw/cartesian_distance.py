00f86dc5b5d41c8b80b04d907edf334b2704275a
Viten3d <90654400+Viten3d@users.noreply.github.com>
2025-11-20 04:03:09 +0000

    clean up

--- cart_dist_hw/cartesian_distance.py
+++ cart_dist_hw/cartesian_distance.py
@@ -1,4 +1,13 @@
-# [one-argument]
+# square root function from calc.py (used in line 30)
+def sqrt(temp):
+    if temp == 0: # protect from "0 problem"
+        return 0
+    g = 1
+    for k in range (0, 100):
+        g = (g + (temp / g)) / 2
+    return g
+
+# cartesian distance function
 def cdis(pts=None):
     # print usage info when ran with no argument
     if pts == None:
@@ -18,56 +27,14 @@ def cdis(pts=None):
     
     # plug values from list into distance formula
     else:
-        temp = ((pts[0] - pts[2]) ** 2) + ((pts[1] - pts[3]) ** 2)
-        # use square root function from calc.py
-        g = 1
-        for k in range (0, 100):
-            g = (g + (temp / g)) / 2
-
-    # output distance between points
-    return f"Distance between ({pts[0]},{pts[1]}) and ({pts[2]},{pts[3]}): {round(g, 5)}"
+        dist = sqrt(((pts[0] - pts[2]) ** 2) + ((pts[1] - pts[3]) ** 2))
+        return dist
 
 #"""
-print(cdis())
-print(cdis([2,2,8,6]))
-print(cdis([5,-5,-6,3]))
+print(f"Info:\n{cdis()}")
+print(f"D[(2,2),(8,6)] = {round(cdis([2,2,8,6]),5)}")
+print(f"D[(5,-5),(-6,3)] = {round(cdis([5,-5,-6,3]),5)}")
 print(cdis(1))
 print(cdis([1]))
 print(cdis([1,2,"tre",4]))
 #"""
-
-"""
-# [two-argument]
-def cdis(p1, p2):
-    # only accept 'list' type args
-    if (type(p1) or type(p2)) != list:
-          return f"Argument types must be 'list' -> [x,y]."
-    
-    # collect point coordinates from arg lists
-    else:
-        temp = ((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2)
-        g = 1
-        for k in range (0, 100): # use square root function from calc.py
-            g = (g + (temp / g)) / 2
-
-    # output distance between points
-    return f"Distance between ({p1[0]},{p1[1]}) and ({p2[0]},{p2[1]}): {round(g, 5)}"
-
-print(cdis([2,2],[8,6]))
-print(cdis([5,-5],[-6,3]))
-print(cdis(1,2))
-"""
-"""
-# [four-argument]
-def cdis(x1, y1, x2, y2):
-    tvar0 = (x1 - x2) ** 2
-    tvar1 = (y1 - y2) ** 2
-    tvar2 = tvar0 + tvar1
-    g = 1
-    for k in range (0, 100):
-            g = (g + (tvar2 / g)) / 2
-    return f"Distance between ({x1},{y1}) and ({x2},{y2}): {round(g, 5)}"
-
-print(cdis(2,2,8,6))
-print(cdis(5,-5,-6,3))
-"""
